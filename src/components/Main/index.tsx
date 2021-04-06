c45dff1c41bd89ceee8b13f2dfa603ceefae3067
Denis Toledo <contato@denistoledo.com.br>
2021-04-06 12:48:49 -0300

    added first styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,23 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um  átomo e React Avançado escrito ao lado"
+    />
+
+    <S.Title>React avançado</S.Title>
+
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Compomponents
+    </S.Description>
+
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Imagem de um desenvolvedor codando"
+    />
+  </S.Wrapper>
 )
 
 export default Main
