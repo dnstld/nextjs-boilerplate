cd4cdcd190b8aed3f06e45b41bd7d667cf279809
Denis Toledo <contato@denistoledo.com.br>
2021-04-07 17:08:18 -0300

    text updates

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,23 +1,17 @@
 import * as S from './styles'
 
 const Main = ({
-  title = 'React Avançado',
+  title = 'ReactJS',
   description = 'TypeScript, ReactJS, NextJS e Styled Compomponents'
 }) => (
   <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado"
-    />
+    <S.Logo src="/img/logo.svg" alt="Some alt" />
 
     <S.Title>{title}</S.Title>
 
     <S.Description>{description}</S.Description>
 
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Imagem de um desenvolvedor codando"
-    />
+    <S.Illustration src="/img/hero-illustration.svg" alt="Some alt" />
   </S.Wrapper>
 )
 
